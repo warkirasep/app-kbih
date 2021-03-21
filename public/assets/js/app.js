3fcc8aa8378c3e74067f16b9c8e9aff68e78bf46
Asep Saepuloh <warkirasep.puloh@gmail.com>
2021-03-22 06:14:58 +0700

    commits

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -49888,8 +49888,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/developer-1/php/app-kbih/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/developer-1/php/app-kbih/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Volumes/HDD750/php/app-kbih/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Volumes/HDD750/php/app-kbih/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
